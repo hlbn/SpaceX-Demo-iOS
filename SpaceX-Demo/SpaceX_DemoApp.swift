0a298133741a197be1ebcd8770777e86816bdc6a
hlbn <adamhlubina@gmail.com>
2022-05-26 15:55:16 +0200

    vytvorenie projektu

---
+++ SpaceX-Demo/SpaceX_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_DemoApp.swift
+//  SpaceX-Demo
+//
+//  Created by Adam Hlubina on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
