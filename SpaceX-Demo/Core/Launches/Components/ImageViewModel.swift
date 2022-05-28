840e356a08783ba702b576c45bae3d7d1374a855
hlbn <adamhlubina@gmail.com>
2022-05-28 02:08:12 +0200

    Vytvorenie reusable struct URLImage modelu na zobrazenie obrázkov

--- SpaceX-Demo/Core/Launches/Components/ImageViewModel.swift
+++ SpaceX-Demo/Core/Launches/Components/ImageViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct URLImage: View {
+    let urlString: String
+    @State var data: Data?
+    
+    var body: some View {
+        if let data = data, let uiimage =  UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .frame(width: 120, height: 120)
+        }else{
+            Image("")
+                .resizable()
+                .frame(width: 120, height: 120)
+                .background(Color.gray)
+                .onAppear{
+                    fetchData()
+                }
+        }
+    }
+    private func fetchData(){
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _,_ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
